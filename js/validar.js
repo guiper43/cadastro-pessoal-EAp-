2e1bce8b59232165f5d6b3a1dbd6805c59a3eed1
@guiper43 <guilhermemano321@gmail.com>
2021-10-23 03:37:30 -0300

    API via cep adicionada

--- js/validar.js
+++ js/validar.js
@@ -59,3 +59,69 @@ function cadastrar() {
     alert(" parabens!! cadastro realizado com sucesso.");
 }
 
+    
+function limpa_formulário_cep(){
+    //Limpa valores do formulário de cep.
+    document.getElementById("endereco").value =("");
+    document.getElementById("bairro").value =("");
+    document.getElementById("cidade").value =("");
+    document.getElementById("estado").value=("");
+    
+}
+
+function meu_callback(conteudo){
+if (!("erro" in conteudo)) {
+    //Atualiza os campos com os valores.
+    document.getElementById("endereco").value =(conteudo.logradouro);
+    document.getElementById("bairro").value =(conteudo.bairro);
+    document.getElementById("cidade").value =(conteudo.localidade);
+    document.getElementById("estado").value =(conteudo.uf);
+} 
+else {
+    //CEP não Encontrado.
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function pesquisaCep(valor){
+
+//Nova variável "cep" somente com dígitos.
+var cep = valor.replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado.
+if (cep != "") {
+
+    //Expressão regular para validar o CEP.
+    var validacep = /^[0-9]{8}$/;
+
+    //Valida o formato do CEP.
+    if(validacep.test(cep)) {
+
+        //Preenche os campos com "..." enquanto consulta webservice.
+        document.getElementById("endereco").value ="...";
+        document.getElementById("bairro").value ="...";
+        document.getElementById("cidade").value ="...";
+        document.getElementById("estado").value ="...";
+
+        //Cria um elemento javascript.
+        var script = document.createElement('script');
+
+        //Sincroniza com o callback.
+        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+        //Insere script no documento e carrega o conteúdo.
+        document.body.appendChild(script);
+
+    } 
+    else {
+        //cep é inválido.
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+} 
+else {
+    //cep sem valor, limpa formulário.
+    limpa_formulário_cep();
+}
+};
