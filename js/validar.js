a24933e6c1d23f4e9f38a235c074c097a1e14833
@guiper43 <guilhermemano321@gmail.com>
2021-10-23 14:10:20 -0300

    comentários adicionados

--- js/validar.js
+++ js/validar.js
@@ -1,4 +1,5 @@
-function validarCpf(cpf){
+//criada a função para validar o cpf
+function validarCpf(cpf) {
     cpf = document.getElementById("cpf").value;
     cpf = cpf.replace(/[^\d]/g, "");
     if (cpf.length != 11 ||
@@ -14,12 +15,15 @@ function validarCpf(cpf){
         cpf == "99999999999") {
         return false;
     }
+    //criadas as váriaveis soma e resto
     var resto;
     var soma = 0;
-    for (i = 1; i <= 9; i++) {
-        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
 
+    for (i = 1; i <= 9; i++) {
+        //multiplicamos cada um dos 9 digitos em ordem  decrescente de 10 a 2 e somamos os resultados
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
     }
+
     resto = (soma * 10) % 11;
     if (resto == 10 || resto == 11) {
         resto = 0;
@@ -42,86 +46,86 @@ function validarCpf(cpf){
         return true;
     }
 }
-function verificarCpf(cpf){
-    if(cpf == ""){
+//função que alerta se o cpf é válido
+function verificarCpf(cpf) {
+    if (cpf == "") {
         return false;
     }
-if(!validarCpf(cpf.value)){
-    alert("o cpf digitado é inválido, verifique e tente novamente.");
-    cpf =document.getElementById("cpf").value = "";
-}
-
-
-}
-
-
-function cadastrar() {
-    alert(" parabens!! cadastro realizado com sucesso.");
-}
-
-    
-function limpa_formulário_cep(){
-    //Limpa valores do formulário de cep.
-    document.getElementById("endereco").value =("");
-    document.getElementById("bairro").value =("");
-    document.getElementById("cidade").value =("");
-    document.getElementById("estado").value=("");
-    
-}
-
-function meu_callback(conteudo){
-if (!("erro" in conteudo)) {
-    //Atualiza os campos com os valores.
-    document.getElementById("endereco").value =(conteudo.logradouro);
-    document.getElementById("bairro").value =(conteudo.bairro);
-    document.getElementById("cidade").value =(conteudo.localidade);
-    document.getElementById("estado").value =(conteudo.uf);
-} 
-else {
-    //CEP não Encontrado.
-    limpa_formulário_cep();
-    alert("CEP não encontrado.");
-}
-}
-
-function pesquisaCep(valor){
-
-//Nova variável "cep" somente com dígitos.
-var cep = valor.replace(/\D/g, '');
-
-//Verifica se campo cep possui valor informado.
-if (cep != "") {
-
-    //Expressão regular para validar o CEP.
-    var validacep = /^[0-9]{8}$/;
-
-    //Valida o formato do CEP.
-    if(validacep.test(cep)) {
-
-        //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById("endereco").value ="...";
-        document.getElementById("bairro").value ="...";
-        document.getElementById("cidade").value ="...";
-        document.getElementById("estado").value ="...";
-
-        //Cria um elemento javascript.
-        var script = document.createElement('script');
-
-        //Sincroniza com o callback.
-        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
-
-        //Insere script no documento e carrega o conteúdo.
-        document.body.appendChild(script);
-
-    } 
-    else {
-        //cep é inválido.
-        limpa_formulário_cep();
-        alert("Formato de CEP inválido.");
+    if (!validarCpf(cpf.value)) {
+        alert("o cpf digitado é inválido. verifique e tente novamente!!");
+        cpf = document.getElementById("cpf").value = "";
     }
-} 
-else {
-    //cep sem valor, limpa formulário.
-    limpa_formulário_cep();
+
+
 }
+
+//função que alerta que o cliente foi cadastrado 
+function cadastrar() {
+    alert(" parabéns!! cadastro realizado com sucesso.");
+}
+
+
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById("endereco").value = ("");
+    document.getElementById("bairro").value = ("");
+    document.getElementById("cidade").value = ("");
+    document.getElementById("estado").value = ("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById("endereco").value = (conteudo.logradouro);
+        document.getElementById("bairro").value = (conteudo.bairro);
+        document.getElementById("cidade").value = (conteudo.localidade);
+        document.getElementById("estado").value = (conteudo.uf);
+    }
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function pesquisaCep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/[^\d]/g, "");
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById("endereco").value = "...";
+            document.getElementById("bairro").value = "...";
+            document.getElementById("cidade").value = "...";
+            document.getElementById("estado").value = "...";
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        }
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    }
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
 };
